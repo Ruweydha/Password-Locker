d914bf7b7f5d9295b5eb7c2683751eea2942aec6
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-22 14:49:30 +0300

    Created method for saving user object

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class User:
     Class that generates new instances of users.
     """
     users =[]
-    def __init__(self, user_name, first_name, last_name, password):
+    def __init__(self, first_name, last_name, user_name, password):
         """
         ___init__ method that helps us define proprrties for our objects
         Args:
@@ -16,3 +16,9 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+    def save_user(self):
+        '''
+        save_user method that saves user objects into users
+        '''
+        User.users.append(self)
