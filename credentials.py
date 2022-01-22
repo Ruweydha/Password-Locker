520109d7bf7d131422d2aab70e78cb471f4237a6
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-22 20:28:01 +0300

    Created the method needed in the test cases

--- credentials.py
+++ credentials.py
@@ -27,4 +27,34 @@ class Credentials:
         '''
         delete_password method to delete a saved password from the passwords list
         '''
-        Credentials.passwords.remove(self)        
+        Credentials.passwords.remove(self) 
+
+    @classmethod
+    def find_by_account(cls, accountName) :
+        '''
+        Method that takes in account and returns the password that matches the account
+        Args:
+          account: Account to search for
+        Returns:
+          Password of the mentioned account  
+        ''' 
+        for password in cls.passwords:
+            if password.account == accountName:
+                return password  
+
+    @classmethod
+    def password_exist(cls, accountName) :
+        '''
+        Method that checks if a password exists from the password list
+        ''' 
+        for password in cls.passwords:
+            if password.account == accountName:
+                return True              
+        return False 
+
+    @classmethod
+    def display_passwords(cls):
+        '''
+        Method that displays the passwords list
+        ''' 
+        return cls.passwords          
