c399c3712d897677a78ff16a2abee46d3f33d83d
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-22 14:34:24 +0300

    Wrote test to see if the object being initialized properly

--- user_test.py
+++ user_test.py
@@ -8,11 +8,26 @@ class TestContact(unittest.TestCase):
     Args:
       unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def tearDown(self) :
+        '''
+        tearDown method that does clean up after each test case has run
+        '''
+        User.users = []
+
     def setUp(self) :
         '''
         Set up method to run before each test case.
         '''
         self.new_user = User("Ruweydha", "Abdinoor", "Rads", "1234")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''  
+        self.assertEqual(self.new_user.first_name, "Ruweydha")
+        self.assertEqual(self.new_user.last_name, "Abdinoor")
+        self.assertEqual(self.new_user.user_name, "Rads") 
+        self.assertEqual(self.new_user.password, "1234") 
 
 
 if __name__ == '__main__':
