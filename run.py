e514945078719e2bee0b720364ae3a569fe1c5ea
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-26 17:57:47 +0300

    Enabled feature that generates password for user if they want

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 from user import User
 from credentials import Credentials
+import random
 
 def create_user(first_name, last_name, user_name, password):
     '''
@@ -78,7 +79,12 @@ def display_passwords():
 
 def main():
     print("Welcome to Password locker") 
-    print('*' *50)   
+    print('*' *50)  
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
 
     print("What would you like to do? \n ")
     while True:
@@ -152,7 +158,34 @@ def main():
             f_name = input("First name: ")
             l_name = input("Last name : ")
             username = input("Username: ")
-            password = input("Password: ")
+            choose_password = input("Would you like us to generate a password for you? Y or N ")
+
+            if choose_password.upper() == 'Y':
+                print("Welcome to the Password Generator!")
+                password = ""
+
+                nr_letters= int(input("How many letters would you like in your password? \n")) 
+                nr_symbols = int(input(f"How many symbols would you like? \n"))
+                nr_numbers = int(input(f"How many numbers would you like? \n"))
+                
+
+                for char in range(1, nr_letters +1):
+                   password += random.choice(letters)
+
+                for char in range(1, nr_symbols):
+                   password += random.choice(symbols)    
+
+                for char in range(1, nr_numbers +1):
+                    password += random.choice(numbers)
+
+                print(f"Your password is {password}")   
+
+            elif choose_password.upper() == 'N':
+                password = input("Enter password of choice: ")  
+
+            else:
+                print('Incorrect input')          
+
 
             save_user(create_user(f_name, l_name, username, password))
             print('\n')
